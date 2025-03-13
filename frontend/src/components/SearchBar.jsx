c1f226dfb7d8e785a8351d4fc5e678cc9bbd97f5
rockmel03 <kamalswag2@gmail.com>
2025-03-13 22:26:48 +0530

    feat: implement search functionality in CreateChatForm and enhance SearchBar component

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
-export const SearchBar = ({ placeholder }) => {
+export const SearchBar = ({ placeholder, onChangeHandler }) => {
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      onChangeHandler(input);
+    }, 1000);
+    return () => {
+      clearTimeout(timeOut);
+    };
+  }, [input]);
+
   return (
     <div className="w-full rounded-full bg-zinc-700 text-white">
       <input
         type="search"
         name="search"
         id="search"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
         placeholder={placeholder || "Search here"}
         autoComplete="off"
         className="px-4 py-2 w-full rounded-full outline-none"
